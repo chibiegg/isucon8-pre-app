dcd6e90cae192fd12ed84e46555af733b9604216
Yosuke Sameshima <yosukes@indeed.com>
2018-09-16 11:04:14 +0900

    ゴミ消し

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -191,7 +191,7 @@ func getEvents(all bool) ([]*Event, error) {
 	}
 	defer tx.Commit()
 
-	rows, err := tx.Query("SELECT * FROM events WHERE  ORDER BY id ASC")
+	rows, err := tx.Query("SELECT * FROM events ORDER BY id ASC")
 	if err != nil {
 		return nil, err
 	}
